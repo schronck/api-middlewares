aef599ca67688213940d3dc42cc1c45805c791f6
richardanyalai <gtaricsi@gmail.com>
2021-09-01 15:51:16 +0200

    Added content to tweets

--- api/middlewares/twitter.ts
+++ api/middlewares/twitter.ts
@@ -10,6 +10,8 @@ export class TwitterMiddleWare extends MiddleWare {
       {
         tweetId: JSON.parse(query).tweetId,
         userId: "10765432100123456789",
+        content:
+          "Here is my address: 0xF2712e7114A237625EFC8bBA6a6ed1Bb8b6029c9",
         likes: 541879,
         comments: 1457,
         retweets: 156,
@@ -18,6 +20,8 @@ export class TwitterMiddleWare extends MiddleWare {
       {
         tweetId: JSON.parse(query).tweetId,
         userId: "17486872687268799789",
+        content:
+          "0x58995FaD03158fB9cd64397347bA97714EF8fC12, oh boy I love beans",
         likes: 9198,
         comments: 456,
         retweets: 23,
@@ -26,6 +30,8 @@ export class TwitterMiddleWare extends MiddleWare {
       {
         tweetId: JSON.parse(query).tweetId,
         userId: "68716971191779892146",
+        content:
+          "This -> 0x4958806608D2E3Aa22BD8818B555A0a24fe6c38E is an address",
         likes: 15,
         comments: 2,
         retweets: 0,
