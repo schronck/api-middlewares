5eab5ab851eb8bfd7ef6b5154fc3d35cdd4d92aa
richardanyalai <gtaricsi@gmail.com>
2021-09-03 16:17:57 +0200

    Added auth (linking Twitter ID with address)

--- api/middlewares/twitter.ts
+++ api/middlewares/twitter.ts
@@ -12,6 +12,7 @@ export class TwitterMiddleWare extends MiddleWare {
         userId: "10765432100123456789",
         content:
           "Here is my address: 0xF2712e7114A237625EFC8bBA6a6ed1Bb8b6029c9",
+        auth: [] as Array<string>,
         likes: 541879,
         comments: 1457,
         retweets: 156,
@@ -22,6 +23,7 @@ export class TwitterMiddleWare extends MiddleWare {
         userId: "17486872687268799789",
         content:
           "0x58995FaD03158fB9cd64397347bA97714EF8fC12, oh boy I love beans",
+        auth: [] as Array<string>,
         likes: 9198,
         comments: 456,
         retweets: 23,
@@ -32,12 +34,18 @@ export class TwitterMiddleWare extends MiddleWare {
         userId: "68716971191779892146",
         content:
           "This -> 0x4958806608D2E3Aa22BD8818B555A0a24fe6c38E is an address",
+        auth: [] as Array<string>,
         likes: 15,
         comments: 2,
         retweets: 0,
         hashtags: ["onlyOneHashTag"]
       }
     ][JSON.parse(query).tweetId];
+
+    tweetObj.auth = [
+      tweetObj.userId,
+      tweetObj.content.match(/0x[a-zA-Z0-9]{40}/)![0].toLowerCase()
+    ];
 
     const stringRes = JSON.stringify(tweetObj);
 
